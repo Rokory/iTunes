fd3899cb47e85027b411b33cfadb53cc9d007057
rokory <Roman.Korecky@easyon.at>
2017-03-31 22:16:23 +0200

    Added examples iTunesResetPlayCounts.ps1 and iTunesSync.ps1. Fix: iTunes.psd1 now exports all functions correctly.

---
+++ iTunes/iTunesResetPlayCounts.ps1
@@ -0,0 +1,10 @@
+# This script shows how to use the Reset-ITunesPlayCount cmdlet.
+# It resets the play count for tracks in several playlists,
+# which have not been played for more than one year,
+# and have a play count greater than 5, 4, 2, or 1.
+
+Reset-ITunesPlayCount -PlayListName 'Beschwingt sämtliche' -PlayedDateOnOrBefore (Get-Date).AddYears(-1) -PlayedCountGreaterThan 5
+Reset-ITunesPlayCount -PlayListName 'Vormittag sämtliche' -PlayedDateOnOrBefore (Get-Date).AddYears(-1) -PlayedCountGreaterThan 4
+Reset-ITunesPlayCount -PlayListName 'Nachmittag sämtliche' -PlayedDateOnOrBefore (Get-Date).AddYears(-1) -PlayedCountGreaterThan 2
+Reset-ITunesPlayCount -PlayListName 'Abend sämtliche' -PlayedDateOnOrBefore (Get-Date).AddYears(-1) -PlayedCountGreaterThan 1
+
---
+++ iTunes/iTunesSync.ps1
@@ -0,0 +1,25 @@
+﻿# This script shows, how to use iTunes.psm1 
+# to convert several playlists at once 
+# and renaming them at destination at the same time.
+# 
+# It pipes a list of hashtables, 
+# each containing a playlist name and a new name, 
+# into the Convert-ItunesPlaylist cmdlet.
+# 
+# You can use this script, 
+# by customizing the $destination variable at the beginning, 
+# e. g. with the drive letter of your USB stick,
+# and customizing the playlist names and new names in the hash table.
+# Moreover, you can delete lines from the list or add new ones.
+
+$destination = 'G:\'
+
+@{ playlist = "Beschwingt alle";       newname="Ford1" } ,
+@{ playlist = "Ab geht's alle";        newname="Ford2" },
+@{ playlist = "Vormittag sämtliche";   newname="Ford3" },
+@{ playlist = "Aufgeregt alle";        newname="Ford4" },
+@{ playlist = "Nachmittag sämtliche";  newname="Ford6" },
+@{ playlist = "Lässig alle";           newname="Ford7" },
+@{ playlist = "Beruhigend alle";       newname="Ford8" },
+@{ playlist = "Abend sämtliche";       newname="Ford9"} |
+Convert-ITunesPlaylist -Destination $destination -Verbose
